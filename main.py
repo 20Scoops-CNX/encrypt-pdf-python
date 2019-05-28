d98a30ac0b346f2223a87852eb304554038ad1e8
Jedsada Tiwongvorakul <jt@20scoops.net>
2019-05-28 12:16:23 +0700

    handler get body from request route

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, jsonify
+import os
 
 app = Flask(__name__, static_folder='files')
 
-def encryptFilePDF():
-    pdfFile = open('./files/sample.pdf', 'rb')
+def encryptFilePDF(file_name, password):
+    pathFile = './files/' + file_name
+    pdfFile = open(pathFile, 'rb')
     pdfReader = PdfFileReader(pdfFile)
     pdfWriter = PdfFileWriter()
 
     for pageNum in range(pdfReader.numPages):
         pdfWriter.addPage(pdfReader.getPage(pageNum))
 
-    pdfWriter.encrypt('20scoops')
+    pdfWriter.encrypt(password)
 
-    resultPdf = open('./files/encrypted_output.pdf', 'wb')
-
+    resultPdf = open('./files/' + 'encrypted_file.pdf', 'wb')
     pdfWriter.write(resultPdf)
     resultPdf.close()
+    os.remove(pathFile)
+
     return resultPdf
 
-@app.route("/encrypt-pdf")
-def home():
-    encryptFilePDF()
-    return send_from_directory(directory='files', filename='encrypted_output.pdf')
+@app.route("/encrypt-pdf", methods=['POST'])
+def encryptFileRoute():
+    if not 'file' in request.files:
+        return jsonify({'error': 'no file'}), 400
+    pdf_file = request.files.get('file')
+    file_name = pdf_file.filename
+    mimetype = pdf_file.content_type
+
+    if not mimetype in 'application/pdf':
+        return jsonify({'error': 'bad-type'})
+
+    directory = 'files'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    pdf_file.save(os.path.join(directory, file_name))
+
+    if not 'password' in request.form:
+        return jsonify({'error': 'key password invalid or empty'}), 400
+    password = request.form.get('password')
+    
+    encryptFilePDF(file_name, password)
+
+    return send_from_directory(directory=directory, filename='encrypted_file.pdf')
     
 if __name__ == "__main__":
     app.run(debug=True)
